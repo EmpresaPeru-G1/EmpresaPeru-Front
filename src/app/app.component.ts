3475ef95ae87aabecf362f995b86bc6da030b991
Oscar Heredia <u201913874@upc.edu.pe>
2023-06-25 12:06:50 -0500

    test integration service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EmpresaPeru-Front';
+  list:any = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:8080/api/v1/client/list').subscribe(response => {
+      console.log(response);
+      this.list = response;
+
+    });
+  }
+
 }
